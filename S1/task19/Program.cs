367dbe7fda945c278e62c05e233c2779bb3adbdb
Denis Uvarov <denisuvarov@192.168.1.3>
2023-04-03 22:37:03 -0300

    Added task 19 solution to S1

---
+++ S1/task19/Program.cs
@@ -0,0 +1,15 @@
+﻿/*      Задача 19: 
+Напишите программу, которая принимает на вход 
+ пятизначное число и проверяет, является ли оно палиндромом
+14212 -> нет
+12821 -> да
+23432 -> да       */
+
+Console.WriteLine("Введите пятизначное число");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+
+if (userNumber < 10000 || userNumber > 99999 ) Console.WriteLine("Введите именно пятизначное число");
+
+else if (userNumber/10000 == userNumber % 10 && userNumber/1000%10 == userNumber / 10 % 10) Console.WriteLine("Да, это число палиндром");
+
+else {Console.WriteLine("Это число не палиндром");}
