4bd61855880d03b84310cd79eae651488db2f850
Denis Uvarov <denisuvarov@192.168.1.3>
2023-04-08 19:38:12 -0300

    Added task 30 conditions to S1

---
+++ S1/task30/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задача 30: Напишите программу, которая выводит массив из 8 элементов,
+// заполненный нулями и единицами в случайном порядке.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* 
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    for (int i = 0; i < length; i++)
+    {
+        collection[i] = new Random().Next(0, 2);
+    }
+ 
+}
+ 
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count - 1)
+    {
+        Console.Write($"{col[position]}, ");
+        position++;
+    }
+    Console.Write($"{col[position]}");
+}
+ 
+Console.Clear();
+ 
+int[] array = new int[100];
+FillArray(array);
+PrintArray(array);
+*/
