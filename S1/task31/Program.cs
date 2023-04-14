b78e7f333af7f0af0516ca35b84489ed68ea36c5
Denis Uvarov <denisuvarov@192.168.1.3>
2023-04-14 16:09:10 -0300

    Added functions of  class work to S1

--- S1/task31/Program.cs
+++ S1/task31/Program.cs
@@ -1,59 +1,9 @@
-﻿
-/*
-Функция для считывания числовых значений с консоли:
-
-Console.WriteLine("Введите длинну массива ");
-int n = ReadNumbersFromConsole(); 
-int[] myArray = new int [n];
-
-Console.WriteLine("Введите минимальное значение элемента массива ");
-int min = ReadNumbersFromConsole(); 
- Console.WriteLine("Введите максимальное значение элемента массива ");
-int max = ReadNumbersFromConsole(); 
-
-
-int ReadNumbersFromConsole()
-{
-    string input = Console.ReadLine();
-    return int.Parse(input);
-}
- 
-
-int length = 12; 
-int leftRange = 1;
-int rightRange = 100;
-int[] myArray = GetRandomArray(length, leftRange, rightRange);
-
-Console.WriteLine($"[{string.Join(", ", myArray)}]");
-
-
-int[] GetRandomArray(int length, int leftRange, int rightRange)
-{
-    int[] array = new int [length];
-
-    for (int i = 0; i < array.Length; i++)
-    {
-        array[i] = Random.Shared.Next(leftRange, rightRange);
-        
-    }
-  
-    return array;
-}
-
-
-
-*/
-
-
-
-
-/*ФУНКЦИИ С ВEТВЛЕНИЕМ
-
-
-// Задача 31: Задайте массив из 12 элементов, заполненный случайными числами 
+﻿// Задача 31: Задайте массив из 12 элементов, заполненный случайными числами 
 // из промежутка [-9, 9]. Найдите сумму отрицательных и положительных элементов массива.
  // Например, в массиве [3,9,-8,1,0,-7,2,-1,8,-3,-1,6] сумма положительных чисел равна 29, 
 // сумма отрицательных равна -20.
+
+
 
 int[] array = new int [12];
 FillArray(array);
@@ -110,7 +60,54 @@ for (int i = 0; i < array.Length; i++)
     return sum;
   }
 
+
+
+
+
+
+/*
+int length = 12; 
+int leftRange = -9;
+int rightRange = 9;
+int[] myArray = GetRandomArray(length, leftRange, rightRange);
+int Sum = GetArraySum (myArray); 
+
+Console.WriteLine($"[{string.Join(", ", Sum)}]");
+
+
+int[] GetRandomArray(int length, int leftRange, int rightRange)
+{
+    int[] array = new int [length];
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = Random.Shared.Next(leftRange, rightRange);
+        
+    }
+  
+    return array;
+}
+
+
+
+int[] GetArraySum(int[] myArray);
+{
+    int SumPos = 0;
+    int SumNeg = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+       if (i > 0)
+       {
+          SumPos = SumPos + i;
+       } 
+        
+        else if (i < 0)
+        {
+          SumNeg += i;  
+        }
+    }
+  
+    return (SumPos, SumNeg);
+}
 */
-
-
-
