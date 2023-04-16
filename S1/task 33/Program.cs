20b701a3a6dadfee6930a3bcdda9c24f372cb8c8
Denis Uvarov <denisuvarov@192.168.1.3>
2023-04-16 20:20:32 -0300

    Added task33 solution to class work to S1

---
+++ S1/task 33/Program.cs
@@ -0,0 +1,50 @@
+﻿// Задача 33: Задайте массив. Напишите программу, которая определяет, присутствует ли заданное число в массиве.
+// 4; массив [6, 7, 19, 345, 3] -> нет
+// 3; массив [6, 7, 19, 345, 3] -> да
+
+
+
+
+int[] array = {6, 7, 19, 345, 3};
+PrintArray(array);
+Console.WriteLine("Введите число, которое надо найти");
+int number = ReadNumberFromConsole();
+bool result = FindNumberInArray(array, number); // использование функции
+if (result == true)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
+ 
+bool FindNumberInArray(int[] array, int numberToSearch) // ФУНКИЦЯ посиск числа 
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] == numberToSearch)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+ 
+void PrintArray(int[] array) // ФУНКИЦЯ вывод массива 
+{
+    Console.Write("{");
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        Console.Write($"{array[i]},");
+    }
+    Console.WriteLine(array[^1] + "}");
+}
+ 
+int ReadNumberFromConsole() // ФУНКИЦЯ вывод числа 
+{
+    string input = Console.ReadLine();
+    return int.Parse(input);
+}
+
+
---
+++ S1/task32/Program.cs
@@ -0,0 +1,79 @@
+﻿// Задача 32: Напишите программу замена элементов массива: положительные элементы замените на соответствующие отрицательные, и наоборот.
+// [-4, -8, 8, 2] -> [4, 8, -8, -2] 
+
+
+int[] array = new int [4];   //задаем массив и его значения 
+array [0] = -4; 
+array [1] = -8; 
+array [2] = 8; 
+array [3] = 2;
+PrintArray(array);
+ChangePosIntoNeg(array); 
+PrintArray(array);
+
+ void ChangePosIntoNeg(int[] array)               //замена положительных на отрицательные
+  {
+for (int i = 0; i < array.Length; i++)
+    {
+    array[i] *= -1;        
+    }
+  }
+
+ void PrintArray(int[] array)                     //вывод массива 
+{
+    Console.Write("{");
+    for (int i = 0; i < array.Length -1; i++)
+    {
+    Console.Write ($"{array[i]},");
+    }
+    Console.WriteLine(array[^1] + "}");
+
+}
+
+
+
+ 
+// FillArray(array);
+// PrintArray(array);
+// //  Console.WriteLine($"Сумма положительных чисел = {PosSum(array)} ");
+// //  Console.WriteLine($"Сумма отрицательных чисел = {NegSum(array)} ");
+
+
+// void FillArray(int[] array)             //генерация массива 
+// {
+//    for (int i = 0; i < array.Length; i++)
+//     {
+//       array[i] = new Random().Next(-9, 10);
+//     }
+
+// }
+
+
+ 
+//  void ChangePosIntoNeg(int[] array)               //замена положительных на отрицательные
+//   {
+// for (int i = 0; i < array.Length; i++)
+//     {
+//     array[i] *= -1;        
+//     }
+//   }
+
+
+
+
+//  int NegSum(int[] array)               //сумма отрицательных
+//   {
+//     int sum = 0; 
+// for (int i = 0; i < array.Length; i++)
+//     {
+//         if (array[i] < 0)
+//         {
+//             sum += array[i];
+//         }
+        
+//     }
+//     return sum;
+//   }
+
+
+
