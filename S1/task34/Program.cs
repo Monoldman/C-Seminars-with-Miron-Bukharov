ad19f0779334900fc827323802140dff273986ce
Denis Uvarov <denisuvarov@192.168.1.3>
2023-04-17 23:56:07 -0300

    Added task34  to S1

---
+++ S1/task34/Program.cs
@@ -0,0 +1,69 @@
+﻿/* Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+Напишите программу, которая покажет количество чётных чисел в массиве. */
+
+
+
+        Console.Clear();
+        int[] array = new int[5]; // задаем размер массива
+        
+        
+        
+        
+        FillArray(array, 1, 5); // задаем массив через функцию гененрации массива 
+        
+        
+        
+        PrintArray(array);
+        PrintArray(MultiplyPairs(array));
+
+
+
+
+        void FillArray(int[] array, int min, int max)             //ФУНКЦИЯ генерации рандомного  массива 
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = new Random().Next(min, max + 1);  // +1  тк  Random().Next не включает последнее значение
+            }
+
+        }
+        void PrintArray(int[] array) // ФУНКИЦЯ вывод массива 
+        {
+            Console.Write("{");
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                Console.Write($"{array[i]},");
+            }
+            Console.WriteLine(array[^1] + "}");
+        }
+       
+       
+       
+       
+       
+        int[] MultiplyPairs(int[] array)
+        {
+            int firstPointer = 0;
+            int secondPointer = array.Length - 1;
+            int pairPointer = 0;
+            int[] pairsMultiply = new int[(array.Length +1) / 2];
+            while (firstPointer < secondPointer)
+            {
+                pairsMultiply [pairPointer] = array[firstPointer] * array[secondPointer];
+                firstPointer++;
+                secondPointer--;
+                pairPointer++;
+            }
+            if (firstPointer == secondPointer)
+            {
+                pairsMultiply [pairPointer] = array[firstPointer]; // *1;
+            }
+             return pairsMultiply;
+        }
+
+
+ 
+
+
+
+
