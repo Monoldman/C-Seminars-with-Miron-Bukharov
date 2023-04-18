cbbb900f3d02507fa9f67fe3e63cee4d3619622c
Denis Uvarov <denisuvarov@192.168.1.3>
2023-04-18 00:21:18 -0300

    Added task34 solution to S1 class work

--- S1/class work/Program.cs
+++ S1/class work/Program.cs
@@ -355,3 +355,51 @@ int FindNumbersInArray(int[] array, int min, int max) // ФУНКИЦЯ пере
 
 
 
+/*    Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+Напишите программу, которая покажет количество чётных чисел в массиве. 
+[345, 897, 568, 234] -> 2    */
+
+
+/*
+// ФУНКЦИЯ генерации случайного массива с вводдом чисел (через GetRandomArray) ВЫЗЫВАЮЩАЯ:
+int length = 10;
+int leftRange = 100;
+int rightRange = 1000;
+int[] myArray = GetRandomArray(length, leftRange, rightRange);
+Console.WriteLine($"[{string.Join(", ", myArray)}]");//выводим МАССИВ ДО ИЗМEНЕНИЙ
+AllpositiveNumbers(myArray); 
+int counts = AllpositiveNumbers(myArray);
+Console.WriteLine($"[{string.Join(", ", counts)}]");// выводим РЕЗУЛЬТАТ 
+
+
+int[] GetRandomArray(int length, int leftRange, int rightRange) //ФУНУЦИЯ генерации случайного массива ВЫЗЫВАЕМАЯ 
+{
+    int[] array = new int [length];
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = Random.Shared.Next(leftRange, rightRange);
+        
+    }
+  
+    return array;
+}
+
+int AllpositiveNumbers (int[] array) //ФУНУЦИЯ ОБРАБАТЫВАЮШАЯ МАССИВ
+{
+    int result = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            result += 1;
+        }
+       
+    }               
+    return result;
+}
+ 
+*/
+
+
