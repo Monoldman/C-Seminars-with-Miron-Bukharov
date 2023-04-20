6067e5044c42cfd112eb36a75bf518e0d1a8fb61
Denis Uvarov <denisuvarov@192.168.1.3>
2023-04-20 16:26:19 -0300

    Added task44 solution to S1

--- S1/class work/Program.cs
+++ S1/class work/Program.cs
@@ -659,5 +659,42 @@ bool IsTriangleExist(int a, int b, int c) //Функиця ло гическая
 */
 
 
+/*
+// Задача 42: Напришите программму, котолрая будет преобразовывать десятичное число в двоичное
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+
+Console.Clear();
+
+int decimalNumber = ReadNumberFromConsole("Введите десятичное число");
+Console.WriteLine(DecToInt(decimalNumber));    
+
+
+ int ReadNumberFromConsole(string message = "") //string message = "" -  говорит о необязательном характере записи 
+{
+    if (message!= "")
+        Console.WriteLine(message );
+    string input = Console.ReadLine();
+    return int.Parse(input); 
+}
+
+
+string DecToInt(int decimalNumber) //ФУНКЦИЯ , коиторое возвращает двоичное число
+{   
+    string result = string.Empty; // то же самое что и "" 
+    while(decimalNumber > 0)
+    {
+        int someValue =  decimalNumber % 2;
+        result = someValue.ToString() + result;
+        decimalNumber /= 2;
+    }
+    return result;
+}   
+
+/*
+
+
 
 
---
+++ S1/task44/Program.cs
@@ -0,0 +1,49 @@
+﻿//адача 44: Не используя рекурсию, введите первые N чисел Фибаначи. Первые два числа Фибаначи: 0 и 1.
+// Если N = 5 -> 0,1,1,2,3
+// Если N = 3 -> 0,0,1
+// Если N = 7 -> 0,1,1,2,3,5,8
+
+
+
+Console.Clear();
+
+int n = ReadNumberFromConsole("Введите количество чисел Фибаначи");
+
+int[] fibArray = GetFibNumbers(n);
+PrintArray(fibArray);
+
+
+int[] GetFibNumbers (int n) // ФУНКИЦЯ преобразования массива
+{
+    int[] result = new int[n];
+    if (n >= 2)
+     result[1] = 1;
+    
+    for (int i = 2; i < result.Length; i++)
+    {
+        result[i] = result[i - 1] + result[i - 2];   
+    }
+    return result;   
+} 
+
+ void PrintArray(int[] array) // ФУНКИЦЯ вывод массива 
+        {
+            Console.Write("{");
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                Console.Write($"{array[i]},");
+            }
+            Console.WriteLine(array[^1] + "}");
+        }
+
+ int ReadNumberFromConsole(string message = "") //string message = "" -  говорит о необязательном характере записи 
+{
+    if (message!= "")
+        Console.WriteLine(message );
+    string input = Console.ReadLine();
+    return int.Parse(input); 
+}
+
+
+ 
+
