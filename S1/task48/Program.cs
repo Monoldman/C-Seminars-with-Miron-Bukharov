3b3b4bfe8fe2593bfdb6c1a759646db77c41c07b
Denis Uvarov <denisuvarov@192.168.1.3>
2023-05-02 00:03:54 -0300

    Added task48  to S1

--- S1/task48/Program.cs
+++ S1/task48/Program.cs
@@ -5,3 +5,37 @@
 // 2 3 4 5
 
 
+Console.Write("Enter the number of rows: ");
+int row_num = int.Parse(Console.ReadLine()!);
+Console.Write("Enter the number of columns: ");
+int column_num = int.Parse(Console.ReadLine()!);
+
+
+int[,] mass = MassNums(row_num, column_num);
+Print(mass);
+
+
+int[,] MassNums(int row, int column) // ФУНКЦИЯ созд рандомного массива строки/слолбцы/лев граница/прав граница 
+{
+int[,] array = new int[row, column]; // двумерный массив 
+
+for (int i = 0; i < row; i++) // счеткик, который бегает по строкам
+for (int j = 0; j < column; j++) // счеткик, который бегает по столбцам
+array[i, j] = i+j; //array[i, j] - указывает на каком элементе массива находится счетчик 
+return array;                                 
+}                                             
+
+void Print(int[,] array) // ФУНКЦИЯ красиво выводит преобразованный двумерный массив  
+{
+int row = array.GetLength(0);// возвращает кортеж 
+int column = array.GetLength(1);
+
+for (int i = 0; i < row; i++)
+{
+for (int j = 0; j < column; j++)
+Console.Write($" {array[i, j], 4} "); // не дает слипнуться  
+Console.WriteLine(); // для перевода курсора на следующую строку
+}
+Console.WriteLine();// не дает слипнуться  
+}
+
