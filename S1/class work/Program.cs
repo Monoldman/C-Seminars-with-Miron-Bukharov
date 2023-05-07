37581c0909d63136dde7254b26aa617025074ae5
Denis Uvarov <denisuvarov@192.168.1.2>
2023-05-06 21:38:11 -0300

    Added task 53 solution to S1

--- S1/class work/Program.cs
+++ S1/class work/Program.cs
@@ -1469,6 +1469,60 @@ double[] FindAverage(int[,] array) // ФУНКЦИЯ находит средне
 */
 
 
+/*
+// Задача 53: Задайте двумерный массив. Напишите программу,
+// которая поменяет местами первую и последнюю строку
+// массива.
+
+
+
+
+Console.Clear(); //очищает консоль
+
+int[,] array = new int[4, 4];
+GetRandomArray(array); 
+PrintArray(array);
+ReplaceFirstLastRows(array); 
+PrintArray(array);
+// Console.WriteLine(string.Join("  ", FindAverage(array)));
+
+
+void PrintArray(int[,] array) // ФУНКЦИЯ выводит преобразованный двумерный массив  
+{
+int row = array.GetLength(0);// возвращает кортеж 
+int column = array.GetLength(1);
+
+for (int i = 0; i < row; i++)
+{
+for (int j = 0; j < column; j++)
+Console.Write($" {array[i, j], 2} "); // не дает слипнуться  
+Console.WriteLine(); // для перевода курсора на следующую строку
+}
+Console.WriteLine();// не дает слипнуться  
+}
+
+void GetRandomArray(int[,] array) // ФУНКЦИЯ cсоздает рандомный двумерный массив
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      array[i, j] = new Random().Next(1, 10);
+    }
+  }
+}
+
+void ReplaceFirstLastRows(int[,] array) //Метод меняет первую строку массива и последнюю
+{
+    for (int i = 0; i < array.GetLength(1); i++)
+    {
+        int temp = array[0, i];
+        array[0, i] = array[array.GetLength(0) - 1, i]; // array[array.GetLength(0) - 1, i] - указывает на конкретный элемент массива
+        array[array.GetLength(0) - 1, i] = temp;        // array[array.GetLength(0)-1  количество элементов в массива -1, чтобы указать индекс
+    }
+}
+*/
+
 
 
 
@@ -1476,7 +1530,8 @@ double[] FindAverage(int[,] array) // ФУНКЦИЯ находит средне
 
 
  /*
-// Использованные  ФУНКЦИИ:
+
+// Ф  У  Н  К  Ц  И  И :
 
 
 int[] GetFibNumbers (int n) // ФУНКИЦЯ преобразования массива (Фибаначи)
@@ -1626,23 +1681,6 @@ array[i, j] *= array[i, j];
 }
 }
 
-int SumDiag(int[,] array) // ФУНКЦИЯ выводит сумму числе главной диагонали  
-        {
-            int count = 0;
-            for (int i = 0; i < array.GetLength(0); i ++)
-               {
-                    for (int j = 0; j < array.GetLength(0); j ++)
-                        {
-                            if(i==j)
-                                {
-                                   count = count + array[i,j];
-                                }
-                        }
-
-                }
-            return count;
-        }
-
 void GetRandomDoubleArray(double[,] array) //ФУНКЦИЯ созд рандомного массива c вещественными числами
 {
   for (int i = 0; i < array.GetLength(0); i++)
@@ -1722,7 +1760,32 @@ double[] FindAverage(int[,] array) // ФУНКЦИЯ находит средне
   return averageColumn;
 }
 
+int SumDiag(int[,] array) // ФУНКЦИЯ выводит сумму чисел главной диагонали  
+        {
+            int count = 0;
+            for (int i = 0; i < array.GetLength(0); i ++)
+               {
+                    for (int j = 0; j < array.GetLength(0); j ++)
+                        {
+                            if(i==j)
+                                {
+                                   count = count + array[i,j];
+                                }
+                        }
+
+                }
+            return count;
+        }
+
+void ReplaceFirstLastRows(int[,] array) //Метод меняет первую строку массива и последнюю
+{
+    for (int i = 0; i < array.GetLength(1); i++)
+    {
+        int temp = array[0, i];
+        array[0, i] = array[array.GetLength(0) - 1, i]; // array[array.GetLength(0) - 1, i] - указывает на конкретный элемент массива
+        array[array.GetLength(0) - 1, i] = temp;        // array[array.GetLength(0)-1  количество элементов в массива -1, чтобы указать индекс
+    }
+}
 
 */
-
 
---
+++ S1/task53/Program.cs
@@ -0,0 +1,51 @@
+﻿// Задача 53: Задайте двумерный массив. Напишите программу,
+// которая поменяет местами первую и последнюю строку
+// массива.
+
+
+
+
+Console.Clear(); //очищает консоль
+
+int[,] array = new int[4, 4];
+GetRandomArray(array); 
+PrintArray(array);
+ReplaceFirstLastRows(array); 
+PrintArray(array);
+// Console.WriteLine(string.Join("  ", FindAverage(array)));
+
+
+void PrintArray(int[,] array) // ФУНКЦИЯ выводит преобразованный двумерный массив  
+{
+int row = array.GetLength(0);// возвращает кортеж 
+int column = array.GetLength(1);
+
+for (int i = 0; i < row; i++)
+{
+for (int j = 0; j < column; j++)
+Console.Write($" {array[i, j], 2} "); // не дает слипнуться  
+Console.WriteLine(); // для перевода курсора на следующую строку
+}
+Console.WriteLine();// не дает слипнуться  
+}
+
+void GetRandomArray(int[,] array) // ФУНКЦИЯ cсоздает рандомный двумерный массив
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      array[i, j] = new Random().Next(1, 10);
+    }
+  }
+}
+
+void ReplaceFirstLastRows(int[,] array) //Метод меняет первую строку массива и последнюю
+{
+    for (int i = 0; i < array.GetLength(1); i++)
+    {
+        int temp = array[0, i];
+        array[0, i] = array[array.GetLength(0) - 1, i]; // array[array.GetLength(0) - 1, i] - указывает на конкретный элемент массива
+        array[array.GetLength(0) - 1, i] = temp;        // array[array.GetLength(0)-1  количество элементов в массива -1, чтобы указать индекс
+    }
+}
