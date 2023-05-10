819e88bbe4b503564958306243d48827e8ec789b
Denis Uvarov <denisuvarov@192.168.1.2>
2023-05-09 23:53:31 -0300

    Added task 67 to S1

--- S1/task67/Program.cs
+++ S1/task67/Program.cs
@@ -3,4 +3,42 @@
 // 453 -> 12
 // 45 -> 9
 
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(SumDigits(num));
+
+int SumDigits(int n)
+{
+if (n == 0) return 0;
+else return n % 10 + SumDigits(n / 10);
+}
+
+// Console.WriteLine("Введите число: "); // решение без рекурсии
+// int number = GetIntInput();
+// Console.WriteLine($"Сумма чисел: {AmountDigits(number)}");
+
+// int GetIntInput()
+// {
+// string input = Console.ReadLine();
+// int result = int.Parse(input);
+// return result;
+// }
+
+// int AmountDigits(int a)
+// {
+// int sum = 0;
+// int b = 0;
+// while (a >= 10)
+// {
+// b = a % 10;
+// sum = sum + b;
+// a = a / 10;
+// }
+// if (a < 10)
+// {
+// sum = sum + a;
+// }
+// return sum;
+// }
 
