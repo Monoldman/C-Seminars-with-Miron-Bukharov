a50d626c61de9ab8dfb8925ff7435728e3277ab3
Denis Uvarov <denisuvarov@192.168.1.2>
2023-05-10 13:54:55 -0300

    Added task 64 to S1

---
+++ S1/task64/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа 
+// в промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+
+
+
+Console.Clear();
+int GetUserNuber(string message) //получить число от пользователя
+{
+  Console.Write(message);
+  int result = Convert.ToInt32(Console.ReadLine());
+  return result;
+}
+
+int n = GetUserNuber("Введите число: ");// проверка числа, является ли оно натуральным
+if (n < 1)
+{
+  Console.WriteLine("Ввдите положительное число!");
+  return;
+}
+
+int NaturalNumber(int n, int m)// МЕТОД рекурсия, числа от 1 до N
+{
+  if (n == m) return n;
+  else Console.Write($"{NaturalNumber(n, m + 1)}, ");
+  return m;
+}
+
+Console.WriteLine(NaturalNumber(n, 1)); // вывод всех числе до 1, включая 1 
+
+
+
+
